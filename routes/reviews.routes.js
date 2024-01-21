205002d239227826d744f6565b3f8c533d3fa41e
PMisar <147728448+PMisar@users.noreply.github.com>
2024-01-21 11:37:27 +0100

    detailing

--- routes/reviews.routes.js
+++ routes/reviews.routes.js
@@ -7,102 +7,121 @@ require("dotenv").config();
 
 const router = express.Router();
 
-router.get("/game/search/:gameName", isAuthenticated, async (req, res) => {
-  const { gameName } = req.params
+// router.get("/game/search/:gameName", isAuthenticated, async (req, res) => {
+//   const { gameName } = req.params
+//   try {
+//     const options =
+//     {
+//       method: 'GET',
+//       url: `https://opencritic-api.p.rapidapi.com/game/search?criteria=${gameName}`,
+//       params: {
+//         sort: 'popularity'
+//       },
+//       headers: {
+//         'X-RapidAPI-Key': process.env.RAPIDAPI_KEY,
+//         'X-RapidAPI-Host': 'opencritic-api.p.rapidapi.com'
+//       }
+//     };
+
+//     const response = await axios.request(options);
+//     const reviews = response.data;
+
+//     res.json(reviews);
+//   } catch (error) {
+//     console.error("Error fetching game reviews:", error.message);
+//     res.status(500).json({ error: "Internal Server Error" });
+//   }
+// });
+
+// router.get("/game/reviews/:gameID", isAuthenticated, async (req, res) => {
+//   const { gameID } = req.params
+//   try {
+//     const options = {
+//       method: 'GET',
+//       url: `https://opencritic-api.p.rapidapi.com/reviews/game/${gameID}`,
+//       params: {
+//         sort: 'popularity'
+//       },
+//       headers: {
+//         'X-RapidAPI-Key': process.env.RAPIDAPI_KEY,
+//         'X-RapidAPI-Host': 'opencritic-api.p.rapidapi.com'
+//       }
+//     };
+
+//     const response = await axios.request(options);
+//     const reviews = response.data;
+
+//     res.json(reviews);
+//   } catch (error) {
+//     console.error("Error fetching game reviews:", error.message);
+//     res.status(500).json({ error: "Internal Server Error" });
+//   }
+// });
+
+// router.get("/game/hall-of-fame", isAuthenticated, async (req, res) => {
+//   try {
+//     const options = {
+//       method: 'GET',
+//       url: 'https://opencritic-api.p.rapidapi.com/game/hall-of-fame',
+//       params: {
+//         sort: 'popularity'
+//       },
+//       headers: {
+//         'X-RapidAPI-Key': process.env.RAPIDAPI_KEY,
+//         'X-RapidAPI-Host': 'opencritic-api.p.rapidapi.com'
+//       }
+//     };
+
+//     const response = await axios.request(options);
+//     const reviews = response.data;
+
+//     res.json(reviews);
+//   } catch (error) {
+//     console.error("Error fetching game reviews:", error.message);
+//     res.status(500).json({ error: "Internal Server Error" });
+//   }
+// });
+
+// router.get("/game/:gameID", isAuthenticated, async (req, res) => {
+//   const { gameID } = req.params;
+//   try {
+//     const options = {
+//       method: 'GET',
+//       url: `https://opencritic-api.p.rapidapi.com/game/${gameID}`,
+//       params: {
+//         sort: 'popularity'
+//       },
+//       headers: {
+//         'X-RapidAPI-Key': process.env.RAPIDAPI_KEY,
+//         'X-RapidAPI-Host': 'opencritic-api.p.rapidapi.com'
+//       }
+//     };
+
+//     const response = await axios.request(options);
+//     const reviews = response.data;
+
+//     res.json(reviews);
+//   } catch (error) {
+//     console.error("Error fetching game reviews:", error.message);
+//     res.status(500).json({ error: "Internal Server Error" });
+//   }
+// });
+// module.exports = router;
+
+router.get("/api/releases/:gameName", isAuthenticated, async (req, res) => {
+  const { gameName } = req.params;
   try {
-    const options = {
-      method: 'GET',
-      url: `https://opencritic-api.p.rapidapi.com/game/search?criteria=${gameName}`,
-      params: {
-        sort: 'popularity'
-      },
-      headers: {
-        'X-RapidAPI-Key': process.env.RAPIDAPI_KEY,
-        'X-RapidAPI-Host': 'opencritic-api.p.rapidapi.com'
-      }
-    };
+    const apiKey = process.env.GAMESPOT_API_KEY; 
+    const apiUrl = `http://www.gamespot.com/api/releases/?api_key=${apiKey}&limit=10`;
 
-    const response = await axios.request(options);
-    const reviews = response.data;
+    const response = await axios.get(apiUrl);
+    const data = response.data;
 
-    res.json(reviews);
+    res.json(data);
   } catch (error) {
-    console.error("Error fetching game reviews:", error.message);
+    console.error("Error fetching game data from Gamespot:", error.message);
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
-router.get("/game/reviews/:gameID", isAuthenticated, async (req, res) => {
-  const { gameID } = req.params
-  try {
-    const options = {
-      method: 'GET',
-      url: `https://opencritic-api.p.rapidapi.com/reviews/game/${gameID}`,
-      params: {
-        sort: 'popularity'
-      },
-      headers: {
-        'X-RapidAPI-Key': process.env.RAPIDAPI_KEY,
-        'X-RapidAPI-Host': 'opencritic-api.p.rapidapi.com'
-      }
-    };
-
-    const response = await axios.request(options);
-    const reviews = response.data;
-
-    res.json(reviews);
-  } catch (error) {
-    console.error("Error fetching game reviews:", error.message);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-});
-
-router.get("/game/hall-of-fame", isAuthenticated, async (req, res) => {
-  try {
-    const options = {
-      method: 'GET',
-      url: 'https://opencritic-api.p.rapidapi.com/game/hall-of-fame',
-      params: {
-        sort: 'popularity'
-      },
-      headers: {
-        'X-RapidAPI-Key': process.env.RAPIDAPI_KEY,
-        'X-RapidAPI-Host': 'opencritic-api.p.rapidapi.com'
-      }
-    };
-
-    const response = await axios.request(options);
-    const reviews = response.data;
-
-    res.json(reviews);
-  } catch (error) {
-    console.error("Error fetching game reviews:", error.message);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-});
-
-router.get("/game/:gameID", isAuthenticated, async (req, res) => {
-  const { gameID } = req.params;
-  try {
-    const options = {
-      method: 'GET',
-      url: `https://opencritic-api.p.rapidapi.com/game/${gameID}`,
-      params: {
-        sort: 'popularity'
-      },
-      headers: {
-        'X-RapidAPI-Key': process.env.RAPIDAPI_KEY,
-        'X-RapidAPI-Host': 'opencritic-api.p.rapidapi.com'
-      }
-    };
-
-    const response = await axios.request(options);
-    const reviews = response.data;
-
-    res.json(reviews);
-  } catch (error) {
-    console.error("Error fetching game reviews:", error.message);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-});
 module.exports = router;
