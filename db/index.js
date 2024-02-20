184d78f29f8378ea3506775c5c488bd215a3119d
PMisar <147728448+PMisar@users.noreply.github.com>
2024-02-20 14:33:13 +0100

    new

--- db/index.js
+++ db/index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 
 const MONGO_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/game-bard-guild";
 
+
 mongoose
   .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
   .then((x) => {
